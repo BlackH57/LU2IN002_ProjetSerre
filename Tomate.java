0b2291a0dfc9669fcbb42765c8a4b0f02d3e39e4
aantoine890 <95503516+aantoine890@users.noreply.github.com>
2021-12-03 22:30:09 +0100

    Add files via upload

---
+++ Tomate.java
@@ -0,0 +1,20 @@
+public class Tomate extends Fruit {
+    private final static double poidsg = (int)(Math.random()*10)+150;
+    private final static double prixkg = 2 ;
+    private static int cpt=1;
+    
+    public Tomate(){
+        super("Tomate"+" n°"+cpt,poidsg,prixkg);
+        System.out.println("un plant de tomate est planté");
+        cpt++;
+    }
+    public double getPrix(){
+        return Tomate.prixkg*Tomate.poidsg/1000;
+    }
+    public int getCpt(){
+        return cpt;
+    }
+
+    
+    
+}
