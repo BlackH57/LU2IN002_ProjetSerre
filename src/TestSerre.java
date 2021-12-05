7ee9bc57282f5e47f6a442294bcdb9b4ad525464
BlackH57 <78650826+BlackH57@users.noreply.github.com>
2021-12-05 23:44:10 +0100

    Update TestSerre.java
    Ajout d'un stockage.

--- src/TestSerre.java
+++ src/TestSerre.java
@@ -1,27 +1,34 @@
 public class TestSerre {
     public static void main(String[] args){
-
-        // création de la Serre
-
-        Serre s = new Serre();
-        
-        // création de vegetaux
-
+    	
+        // creation de la Serre
+        Serre s = new Serre(5);
+        // creation du stockage
+        Stockage stock = new Stockage();
+        // creation de vegetaux
         int nbVegetauxMax = s.taille*s.taille;
-
-        for(int i=0;i<=nbVegetauxMax;i++){
+        
+        Carotte c = new Carotte();
+        
+        for(int i=0;i<nbVegetauxMax;i++){
             s.planter(new Carotte());
         
         }
         
+        System.out.println(s.toString());
         // faire pousser les vegetaux
 
         s.rafraichirSerre();
         s.rafraichirSerre();
-
+        
         // recolter les vegetaux murs
-
-        s.recolter();
+        s.recolter(c, stock);
+        System.out.println(stock.toString());
+        
+        // faire pourrir les vegetaux
+        ///s.rafraichirSerre();
+        ///s.rafraichirSerre();
+        
 
         System.out.println(s.toString());
     }
