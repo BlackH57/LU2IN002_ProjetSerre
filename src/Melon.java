280e6cafb92074b8f7d8b1fc289c72d97b6e5f4b
BlackH57 <78650826+BlackH57@users.noreply.github.com>
2021-12-06 22:19:30 +0100

    Update and rename Tomate.java to Melon.java

--- src/Melon.java
+++ src/Melon.java
@@ -1,15 +1,15 @@
-public class Tomate extends Fruit {
+public class Melon extends Fruit {
     private final static double poidsg = (int)(Math.random()*10)+150;
     private final static double prixkg = 2 ;
     private static int cpt=1;
     
-    public Tomate(){
-        super("Tomate"+" n°"+cpt,poidsg,prixkg,"graine");
-        System.out.println("un plant de tomate est planté");
+    public Melon(){
+        super("Melon"+" n°"+cpt,poidsg,prixkg,"graine");
+        System.out.println("un plant de melon est planté");
         cpt++;
     }
     public double getPrix(){
-        return Tomate.prixkg*Tomate.poidsg/1000;
+        return Melon.prixkg*Melon.poidsg/1000;
     }
     public int getCpt(){
         return cpt;
