d5def4053c459ac1884df3f3dd032d09b217168e
aantoine890 <95503516+aantoine890@users.noreply.github.com>
2021-12-06 22:17:13 +0100

    Update Poireau.java

--- src/Poireau.java
+++ src/Poireau.java
@@ -1,14 +1,14 @@
-public class Poireau extends Legume {
+public class Asperge extends Legume {
     private final static double poidsg = (int)(Math.random()*11)+150;
     private final static double prixkg = 0.90 ;
     private static int cpt=1;
     
-    public Poireau(){
-        super("Poireau"+" n°"+cpt,poidsg,prixkg,"graine");
+    public Asperge(){
+        super("Asperge"+" n°"+cpt,poidsg,prixkg,"graine");
         cpt++;
     }
     public double getPrix(){
-        return Poireau.prixkg*Poireau.poidsg/1000;
+        return Asperge.prixkg*Asperge.poidsg/1000;
     }
     public int getCpt(){
         return cpt;
