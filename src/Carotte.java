1fc9ef895d5dc6ddacc47569076a7f98a130e50e
BlackH57 <78650826+BlackH57@users.noreply.github.com>
2021-12-10 23:47:58 +0100

    Update Carotte.java
    Implémentation des méthodes abstraites de l'interface Hiver

--- src/Carotte.java
+++ src/Carotte.java
@@ -1,4 +1,4 @@
-public class Carotte extends Legume {
+public class Carotte extends Legume implements Hiver {
     private final static double poidsg = (int)(Math.random()*11)+100;;
     private final static double prixkg = 0.65 ;
     private static int cpt=1;
@@ -6,6 +6,25 @@ public class Carotte extends Legume {
     public Carotte(){
         super("Carotte"+" n°"+cpt,poidsg,prixkg,"graine");
         cpt++;
+    }
+    
+    @Override
+    public boolean isHiver(Serre s) {
+        return s.getSaison() == "Hiver";
+    }
+
+    @Override
+    public void pousse(Vegetaux v, Serre s) {
+        if(isHiver(s)){
+            if(Math.random() < 0.8){
+                v.pousser();
+            }
+        }
+        else{
+            if(Math.random() < 0.1){
+                v.pousser();
+            }
+        }
     }
     
 }
