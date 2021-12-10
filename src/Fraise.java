49a3da9cb611c79efd7ea5fd1905f15141be6241
BlackH57 <78650826+BlackH57@users.noreply.github.com>
2021-12-10 23:49:55 +0100

    Update Fraise.java
    Implementation des methodes abstraites des interfaces Printemps et Ete

--- src/Fraise.java
+++ src/Fraise.java
@@ -1,4 +1,4 @@
-public class Fraise extends Fruit{
+public class Fraise extends Fruit implements Printemps, Ete {
     private final static double poidsg = (int)(Math.random()*2)+10;
     private final static double prixkg = 7 ;
     private static int cpt=1;
@@ -14,6 +14,29 @@ public class Fraise extends Fruit{
     public int getCpt(){
         return cpt;
     }
+    
+    
+    @Override
+    public boolean isPrintemps(Serre s) {
+        return s.getSaison() == "Printemps";
+    }
 
+    public boolean isEte(Serre s) {
+        return s.getSaison() == "Ete";
+    }
+
+    @Override
+    public void pousse(Vegetaux v, Serre s) {
+        if(isPrintemps(s) || isEte(s)){
+            if(Math.random() < 0.8){
+                v.pousser();
+            }
+        }
+        else{
+            if(Math.random() < 0.1){
+                v.pousser();
+            }
+        }
+    }
     
 }
