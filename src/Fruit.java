cc964de1a05378a87cf91c4bd820e24c8dc95024
BlackH57 <78650826+BlackH57@users.noreply.github.com>
2021-12-13 22:14:15 +0100

    Update Fruit.java
    Ajout de la méthode abstraite de Vegetaux pousse

--- src/Fruit.java
+++ src/Fruit.java
@@ -3,4 +3,6 @@ public abstract class Fruit extends Vegetaux{
     public Fruit(String nom,double poids,double prixPoids,String etat){
         super(nom,poids,prixPoids,etat);
     }
+    
+    public abstract void pousse(Vegetaux v, Serre s);
 }
