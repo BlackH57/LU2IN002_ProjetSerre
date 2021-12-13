891f9544d55d84dc81f4c2a4a3479f6843ffa2b3
BlackH57 <78650826+BlackH57@users.noreply.github.com>
2021-12-13 21:38:21 +0100

    Update TestSerre.java
    Meilleur class de test

--- src/TestSerre.java
+++ src/TestSerre.java
@@ -1,36 +1,48 @@
 public class TestSerre {
     public static void main(String[] args){
-    	
+        
+        int taille = 10;
+
         // creation de la Serre
-        Serre s = new Serre(5);
-        // creation du stockage
-        Stockage stock = new Stockage();
+        Serre s = Serre.creationSerre(taille, "Hiver");
+        // creation de l'organisation
+        Organisation org = new Organisation();
         // creation de vegetaux
         int nbVegetauxMax = s.taille*s.taille;
-        
-        Carotte c = new Carotte();
-        
-        for(int i=0;i<nbVegetauxMax;i++){
+
+        for(int i=0;i<nbVegetauxMax ;i++){
             s.planter(new Carotte());
-        
         }
         
-        System.out.println(s.toString());
-        // faire pousser les vegetaux
+        int nbTours = 7;
 
-        s.rafraichirSerre();
-        s.rafraichirSerre();
-        
-        // recolter les vegetaux murs
-        s.recolter(c, stock);
-        System.out.println(stock.toString());
-        
-        // faire pourrir les vegetaux
-        ///s.rafraichirSerre();
-        ///s.rafraichirSerre();
-        
+        for(int i = 0; i<nbTours; i++){
+            System.out.println("Tour "+i+" : \n" + s.toString());
+            try {
+                s.recolter(new Carotte(), org.getStockage());
+                s.nextTour();
+            } catch (VegetauxException e){
+                System.out.println("Pas le bon type d'objet");
+                return;
+            }
+        }
 
-        System.out.println(s.toString());
+        System.out.println(org.getStockage());
+        int len = org.getStockage().getStockeurSize();
+
+        for(int i = 0; i<len;i++){
+            try{
+                double benef = org.vente(new Carotte());
+                System.out.println("vente de carotte : +" + benef);
+            }
+            catch(VegetauxException e){
+                System.out.println("Erreur: au tour" + i +" \ntaille : " + org.getStockage().getStockeurSize() + ", recette : " + org.getRecettes());
+                break;
+            }
+            
+        }
+
+        System.out.println("recette : " + org.getRecettes());
     }
     
 }
